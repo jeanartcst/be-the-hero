c3e797eb40c47f925bc6436a5ab7690efa320581
Jean <jeanartcst@gmail.com>
2020-03-29 18:13:08 -0300

    Insert, do front-end e correções do back-end

--- backend/src/database/migrations/20200328113017_create_incidents.js
+++ backend/src/database/migrations/20200328113017_create_incidents.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('incidents', function (table){
-        table.increments();
-        table.string('tittle').notNullable();
+      table.increments(); 
+        table.string('title').notNullable();
         table.string('description').notNullable();
         table.decimal('value').notNullable();
         
-        table.string('ong_id').notNullable();
-
+        table.string('ong_id').notNullable(); 
+ 
         table.foreign('ong_id').references('id').inTable('ongs');
     });
     };
